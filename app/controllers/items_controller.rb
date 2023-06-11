efc9bff4498ad8bed80195d7a9a0e41f393fc20a
rmrmrmr <ricardomartineza77@gmail.com>
2023-06-10 19:54:27 -0600

    Create groups & items routes (View and Controller)

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,5 @@
+class ItemsController < ApplicationController
+  def new; end
+
+  def create; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "splash#index"
 
-  resources :splash, only: [:index]
+  resources :splash, only: [:index] do
+    resources :groups, only: [:index, :new, :create, :show] do
+      resources :items, only: [:new, :create]
+    end
+  end
 end
