a935b30fc45fa15f28163bd77e966f0b307a369f
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-14 11:21:29 -0600

    Fix rubocop errors

--- spec/views/groups_show_spec.rb
+++ spec/views/groups_show_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Groups show', type: :feature do
     @item1 = Item.create(name: 'itemOne', amount: 10, user_id: @user.id, group_id: @category1.id)
     @item2 = Item.create(name: 'itemTwo', amount: 20, user_id: @user.id, group_id: @category1.id)
     # creates a group not owned by the user
-    @category3 = Group.create(name: 'group3', icon: 'twoIcon.svg', user_id: @user.id+1)
+    @category3 = Group.create(name: 'group3', icon: 'twoIcon.svg', user_id: @user.id + 1)
     # creates an item not owned by the user
-    @item3 = Item.create(name: 'itemThree', amount: 30, user_id: @user.id+1, group_id: @category3.id)
+    @item3 = Item.create(name: 'itemThree', amount: 30, user_id: @user.id + 1, group_id: @category3.id)
     visit unauthenticated_root_path
     click_on(id: 'login')
     fill_in 'Email', with: @user.email
@@ -20,12 +20,12 @@ RSpec.describe 'Groups show', type: :feature do
   end
 
   it 'Has transactions title' do
-    expect(page).to have_content('TRANSACTIONS')  
+    expect(page).to have_content('TRANSACTIONS')
   end
 
   it 'Has name of Items' do
-    expect(page).to have_content('itemOne')  
-    expect(page).to have_content('itemTwo')  
+    expect(page).to have_content('itemOne')
+    expect(page).to have_content('itemTwo')
   end
 
   it 'displays the correct amount of each item' do
@@ -34,6 +34,6 @@ RSpec.describe 'Groups show', type: :feature do
   end
 
   it 'displays the correct total amount' do
-    expect(page).to have_content('30.0')  
+    expect(page).to have_content('30.0')
   end
 end
--- spec/views/items_new_spec.rb
+++ spec/views/items_new_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Groups New', type: :feature do
     @category1 = Group.create(name: 'group1', icon: 'oneIcon.svg', user_id: @user.id)
     @category2 = Group.create(name: 'group2', icon: 'twoIcon.svg', user_id: @user.id)
     # creates a group not owned by the user
-    @category3 = Group.create(name: 'group3', icon: 'twoIcon.svg', user_id: @user.id+1)
+    @category3 = Group.create(name: 'group3', icon: 'twoIcon.svg', user_id: @user.id + 1)
     visit unauthenticated_root_path
     click_on(id: 'login')
     fill_in 'Email', with: @user.email
